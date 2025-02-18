6ff7bf52570f21dca82a705a2ec2d11370556c3e
tayyabasamad <tayyaba.samad8@gmail.com>
2025-02-18 17:41:04 +0500

    veg and days done

---
+++ days.js
@@ -0,0 +1,18 @@
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const daysContainer = document.getElementById("daysContainer");
+days.forEach(day => {
+    let card = document.createElement("div");
+    card.className = "card";
+    card.textContent = day;
+    daysContainer.appendChild(card);
+});
+
+const monthsContainer = document.getElementById("monthsContainer");
+months.forEach(month => {
+    let card = document.createElement("div");
+    card.className = "card";
+    card.textContent = month;
+    monthsContainer.appendChild(card);
+});
